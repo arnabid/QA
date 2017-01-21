582514fbc2e13543f3f524ed3d4aef8c16d07363
Arnab <arnab@Arnabs-MacBook-Pro.local>
2017-01-21 08:21:46 -0600

    Add city lockers question

---
+++ arrays/citylockers.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 08:55:12 2016
+
+@author: arnab
+"""
+import itertools
+
+def solution1(cityLength, cityWidth, lockerXCoordinates, lockerYCoordinates):
+    
+    # define the output grid
+    maxdist = cityLength+cityWidth-2
+    grid = [[maxdist for i in xrange(cityWidth)] for i in xrange(cityLength)]
+    
+    for lockerX, lockerY in itertools.izip(lockerXCoordinates, lockerYCoordinates):
+        grid[lockerX-1][lockerY-1] = 0
+
+    for i in xrange(cityLength):
+        for j in xrange(cityWidth):
+            # get the locker locations
+            if grid[i][j] != 0:
+                for lockerX, lockerY in itertools.izip(lockerXCoordinates, lockerYCoordinates):
+                    dist = abs(i-lockerX+1) + abs(j-lockerY+1)
+                    grid[i][j] = min(grid[i][j], dist)
+    
+    print (grid)
+
+def solution(cityLength, cityWidth, lockerXCoordinates, lockerYCoordinates):
+    
+    # define the output grid
+    grid = [[float('inf') for i in xrange(cityWidth)] for i in xrange(cityLength)]
+    
+    #numLockers = len(lockerXCoordinates)
+    for i in xrange(1,cityLength+1):
+        for j in xrange(1,cityWidth+1):
+            # get the locker locations
+            for lockerX, lockerY in itertools.izip(lockerXCoordinates, lockerYCoordinates):
+            #for index in xrange(numLockers):
+                #lockerX = lockerXCoordinates[index]
+                #lockerY = lockerYCoordinates[index]
+                # get the manhattan distance to this locker from (i,j)
+                mdist = abs(i-lockerX) + abs(j-lockerY)
+                grid[i-1][j-1] = min(grid[i-1][j-1], mdist)
+                # if the grid location is a locker, skip remaining lockers
+                if grid[i-1][j-1] == 0:
+                    break
+    
+    print (grid)
+
+if __name__ == '__main__':
+    cityLength = 4
+    cityWidth = 5
+    lockerXCoordinates = [2,3,1]
+    lockerYCoordinates = [2,4,1]
+    solution(cityLength,cityWidth,lockerXCoordinates,lockerYCoordinates)
+    print ("=====")
+    solution1(cityLength,cityWidth,lockerXCoordinates,lockerYCoordinates)
+
+    
+            
