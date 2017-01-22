81fab185a3695d05eaa9b5f61f22ef7cf9c0b13c
Arnab <arnab@Arnabs-MacBook-Pro.local>
2017-01-21 19:26:41 -0600

    Add extension in snakes and ladders problem Print the path to the top and the corresponding sequence of dice rolls.

--- arrays/snakesAndLadders.py
+++ arrays/snakesAndLadders.py
@@ -12,20 +12,35 @@ problem: https://www.hackerrank.com/challenges/the-quickest-way-up
 from collections import Counter
 import Queue
 
-def solution(source, end):
+def printSolution(graph, edgeTo, start, end):
+    path, rolls = [], []
+    while end != start:
+        path.append(end)
+        rolls.append(graph[edgeTo[end]][end])
+        end = edgeTo[end]
+    path.append(start)
+    path.reverse()
+    rolls.reverse()
+    print ("Quickest way up is " + "->".join(str(node) for node in path))
+    print ("The sequence of dice rolls is " + ",".join(str(roll) for roll in rolls))
+
+def solution(graph, start, end):
     dlabels = Counter()
-    dlabels[source] = 0
+    edgeTo = Counter()
+    dlabels[start] = 0
     q = Queue.Queue()
-    q.put(source)
+    q.put(start)
         
     while not q.empty():
         v = q.get()
         if v == end:
+            printSolution(graph, edgeTo, start, end)
             return dlabels[v]
         for w in graph[v]:
             if w not in dlabels:
                 q.put(w)
                 dlabels[w] = 1 + dlabels[v]
+                edgeTo[w] = v
     return -1
 
 if __name__ == '__main__':
@@ -41,7 +56,13 @@ if __name__ == '__main__':
         for snake in xrange(ns):
             u, v = map(int, raw_input().strip().split(" "))
             nodes[u] = v
-        # make the graph
+        """ make the graph; graph is a dict of dicts
+        graph[u][v] = w
+        to reach v from u, roll w on the dice
+        """
         for i in xrange(1, 100):
-            graph[nodes[i]] = [nodes[x] for x in xrange(nodes[i]+1, min(nodes[i]+7,101) )]
-        print (solution(1, 100))
+            graph[nodes[i]] = Counter()
+            for x in xrange(nodes[i]+1, min(nodes[i]+7,101) ):
+                graph[nodes[i]][nodes[x]] = x - nodes[i]
+            #graph[nodes[i]] = [nodes[x] for x in xrange(nodes[i]+1, min(nodes[i]+7,101) )]
+        print (solution(graph, 1, 100))
