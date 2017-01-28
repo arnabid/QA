dd2804443f7b14b8ec483dc562ac532da8561678
Arnab <arnab@Arnabs-MacBook-Pro.local>
2017-01-27 18:27:55 -0600

    Add base case and account for holes in ground

--- stack/collectRainWater.py
+++ stack/collectRainWater.py
@@ -6,15 +6,21 @@ Created on Mon Oct 24 08:04:01 2016
 """
 """
 Find the amount of water collected on the top of bars
+account for holes; -ve values allowed
 """
 
-def findwater(bars, n):
-    leftmax = [0]
-    # leftmax[i] - stores the max bar seen to the left of i excluding i
-    # no water will be collected on bars[0] and bars[n-1]
+def findwater(bars):
+    
+    # return 0 if empty list
+    if not bars:
+        return 0
+
+    n = len(bars)
+    leftmax = [0] * n
+    # leftmax[i] - the max bar to the left of bar i excluding i
 
     for i in xrange(1,n-1):
-        leftmax.append(max(leftmax[i-1], bars[i-1]))
+        leftmax[i] = max(leftmax[i-1], bars[i-1])
 
     rmax, total = bars[-1], 0
 
@@ -23,12 +29,17 @@ def findwater(bars, n):
             total += min(leftmax[i], rmax) - bars[i]
         if bars[i] > rmax:
             rmax = bars[i]
+    
+    if bars[0] < 0:
+        total += abs(bars[0])
+    
+    if bars[-1] < 0:
+        total += abs(bars[-1])
 
     return total
 
 
 if __name__ == '__main__':
     bars = map(int, raw_input().strip().split(" "))
-    n = len(bars)
     
-    print (findwater(bars,n))
+    print (findwater(bars))
