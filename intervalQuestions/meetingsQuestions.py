218b33c4161ba0b0410d03bab48aefc550b82b41
Arnab Saha <arnab.saha.id@gmail.com>
2017-05-01 17:30:44 -0500

    Add a more efficient method to find the min rooms required to hold meetings

--- intervalQuestions/meetingsQuestions.py
+++ intervalQuestions/meetingsQuestions.py
@@ -6,6 +6,7 @@ Created on Mon Jan 16 21:04:58 2017
 """
 
 import random
+from collections import Counter
 
 """
 maxNumberMeetings - maximum number of meetings that can be
@@ -44,19 +45,20 @@ def maxNumberMeetings2(meetings):
 """
 minRooms - minimum number of rooms required to schedule
 list of meetings
+minRooms = maximum number of active meetings at any given time
+time - nlogn; space - n
 """
 def minRooms(meetings):
     events = []
     for meeting in meetings:
         events += [(meeting[0], 1), (meeting[1], -1)]
-    
-    # events holds the start and stop times of all meetings
-    # in increasing order, for start and stop times coinciding, stop times are 
-    # placed first
-    """
-    minRooms = maximum number of active meetings at any given time
-    """
     events.sort(key = lambda x: (x[0], x[1]))
+    
+    """
+    events holds the start and stop times of all meetings
+    in increasing order, for start and stop times coinciding, stop times are 
+    placed first
+    """
     result, occupiedrooms = 0, 0
     for event in events:
         if event[1] == 1:
@@ -68,14 +70,40 @@ def minRooms(meetings):
             occupiedrooms -= 1
     return result
 
+"""
+time - n; space - constant
+"""
+def calcRooms(meetings, start = None, end = None):
+    mnStart, mxEnd = 25, -1
+    mapM = Counter()
+    for meeting in meetings:
+        mapM[meeting[0]] += 1
+        mapM[meeting[1]] -= 1
+        mnStart = min(mnStart, meeting[0])
+        mxEnd = max(mxEnd, meeting[1])
+    
+    if start is None:
+        start = mnStart
+    
+    if end is None:
+        end = mxEnd
+    
+    activeMeetings, result = 0, 0
+    for i in xrange(start, end+1):
+        if i in mapM:
+            activeMeetings += mapM[i]
+            result = max(result, activeMeetings)
+    return result
+        
+
 if __name__ == '__main__':
     meetings = []
-    for i in xrange(5):
-        start = random.randint(1,9)
-        end = random.randint(start+1,11)
+    for i in xrange(50):
+        start = random.randint(1,7)
+        end = random.randint(start+3,11)
         meetings.append([start,end])
-    print (meetings)
-    print (maxNumberMeetings(meetings))
-    print (meetings)
-    print (maxNumberMeetings2(meetings))
-    #print (minRooms(meetings))
+#    print (meetings)
+#    print (maxNumberMeetings(meetings))
+#    print (meetings)
+#    print (maxNumberMeetings2(meetings))
+    print (minRooms(meetings), calcRooms(meetings))
