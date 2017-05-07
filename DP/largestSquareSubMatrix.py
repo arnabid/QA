f313bf4852e2939a349b5438f435463e9ad00df9
Arnab Saha <arnab.saha.id@gmail.com>
2017-05-07 17:20:32 -0500

    Add question to find the largest square sub-matrix with all 1s

---
+++ DP/largestSquareSubMatrix.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 17:01:46 2017
+
+@author: arnab
+"""
+
+"""
+Maximum size square sub-matrix with all 1s
+reference: http://www.geeksforgeeks.org/maximum-size-sub-matrix-with-all-1s-in-a-binary-matrix/
+"""
+import random
+
+def sol(arr):
+    r, c = len(arr), len(arr[0])
+    s = [[0 for i in xrange(c)] for i in xrange(r)]
+    ans = arr[0][0]
+    maxi = 0
+    maxj = 0
+    
+    # copy the first row
+    for i in xrange(c):
+        s[0][i] = arr[0][i]
+        if s[0][i] > ans:
+            ans = s[0][i]
+            maxi = 0
+            maxj = i
+    
+    # copy the first column
+    for i in xrange(r):
+        s[i][0] = arr[i][0]
+        if s[i][0] > ans:
+            ans = s[i][0]
+            maxi = i
+            maxj = 0
+    
+    for i in xrange(1, r):
+        for j in xrange(1, c):
+            if arr[i][j] == 1:
+                s[i][j] = min(s[i][j-1], s[i-1][j-1], s[i-1][j]) + 1
+                if s[i][j] > ans:
+                    ans = s[i][j]
+                    maxi = i
+                    maxj = j
+            else:
+                s[i][j] = 0
+    
+    return ans, (maxi-ans+1, maxj-ans+1), (maxi, maxj)
+
+if __name__ == '__main__':
+    arr = [[random.randint(0,1) for i in xrange(5)] for i in xrange(5)]
+    print (arr)
+    print (sol(arr))
