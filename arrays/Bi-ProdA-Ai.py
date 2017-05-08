4d98d35ede01c4a4ed067453c829076bc159ad85
Arnab Saha <arnab.saha.id@gmail.com>
2017-05-08 09:50:19 -0500

    Add description to Bi-ProdA-Ai.py

--- arrays/Bi-ProdA-Ai.py
+++ arrays/Bi-ProdA-Ai.py
@@ -5,6 +5,14 @@ Created on Mon May  8 09:19:18 2017
 @author: arnab
 """
 
+"""
+Given an array A, how would you create an array B where for each i from 0 to n, 
+b[i] = a[0] * a[1] * ... * a[n-1] /a[i] 
+You should do this in O(n) time without using division. 
+
+As an example, if A = {1,2,3,4}, B should be (24,12,8,6).
+"""
+
 def sol(A):
     n = len(A)
     B = [1] * n
