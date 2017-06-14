5d560f098a3950a6f6395249a6fac4394a1f5118
Arnab Saha <arnab.saha.id@gmail.com>
2017-06-14 18:05:58 -0500

    make minor code revisions

--- DP/largestSquareSubMatrix.py
+++ DP/largestSquareSubMatrix.py
@@ -14,37 +14,29 @@ import random
 def sol(arr):
     r, c = len(arr), len(arr[0])
     s = [[0 for i in xrange(c)] for i in xrange(r)]
-    ans = arr[0][0]
-    maxi = 0
-    maxj = 0
+    ans, maxi, maxj = arr[0][0], 0, 0
     
     # copy the first row
     for i in xrange(c):
         s[0][i] = arr[0][i]
         if s[0][i] > ans:
-            ans = s[0][i]
-            maxi = 0
-            maxj = i
+            ans, maxi, maxj = 1, 0, i
     
     # copy the first column
     for i in xrange(r):
         s[i][0] = arr[i][0]
         if s[i][0] > ans:
-            ans = s[i][0]
-            maxi = i
-            maxj = 0
+            ans, maxi, maxj = 1, i, 0
     
     for i in xrange(1, r):
         for j in xrange(1, c):
             if arr[i][j] == 1:
                 s[i][j] = min(s[i][j-1], s[i-1][j-1], s[i-1][j]) + 1
                 if s[i][j] > ans:
-                    ans = s[i][j]
-                    maxi = i
-                    maxj = j
-            else:
-                s[i][j] = 0
+                    ans, maxi, maxj = s[i][j], i, j
     
+    # return the size of the largest square sub-matrix
+    # and the top left and bottom right corner
     return ans, (maxi-ans+1, maxj-ans+1), (maxi, maxj)
 
 if __name__ == '__main__':
