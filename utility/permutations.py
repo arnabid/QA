df217b160bc452b42a2bb65ab9eca420f770f358
Arnab Saha <arnab.saha.id@gmail.com>
2017-06-13 21:48:37 -0500

    Revise the permutations question, clean up

--- utility/permutations.py
+++ utility/permutations.py
@@ -7,44 +7,32 @@ Created on Sun Mar  5 09:32:04 2017
 
 """
 print permutations of a string
-reference: http://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
 http://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
 """
 
-def permute1(a, l, r):
-    if l == r:
-        print (''.join(a))
-    else:
-        for i in xrange(l,r+1):
-            a[l], a[i] = a[i], a[l]
-            permute1(a, l+1, r)
-            a[l], a[i] = a[i], a[l]
-            
-def permute2(prefix, s, perms):
-    """
-    perms holds only distinct permutations
-    """
+def perm1(s):
+    perm1UT("", s)
+
+def perm1UT(prefix, s):
     n = len(s)
     if n == 0:
-        perms.add(prefix)
-    else:
-        for i in xrange(n):
-            permute2(prefix+s[i], s[0:i]+s[i+1:n], perms)
+        print (prefix)
+    for i in xrange(n):
+        perm1UT(prefix + s[i], s[:i] + s[i+1:])
 
-def permute3(arr, n):
-    if n == 1:
-        print (''.join(arr))
-    else:
-        for i in xrange(n):
-            arr[n-1], arr[i] = arr[i], arr[n-1]
-            permute3(arr, n-1)
-            arr[n-1], arr[i] = arr[i], arr[n-1]
+def perm2(s, k):
+    perm2UT("", s, k)
 
+def perm2UT(prefix, s, k):
+    if k == 0:
+        print (prefix)
+        return
+    for i in xrange(len(s)):
+        perm2UT(prefix + s[i], s[:i] + s[i+1:], k-1)
 
 if __name__ == '__main__':
-    s = "ABCA"
-    a = list(s)
-    #permute1(a, 0, len(a)-1)
-    perms = set()
-    permute2("", s, perms)
-    print perms, len(perms)
+    s = "ABC"
+    perm1(s)
+    
+    k = 2
+    perm2(s, 2)
