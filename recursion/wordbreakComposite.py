f80fc3220a580c11cfeed922702358f48a9beec7
Arnab Saha <arnab.saha.id@gmail.com>
2017-07-08 18:26:44 -0500

    refactor code

--- recursion/wordbreakComposite.py
+++ recursion/wordbreakComposite.py
@@ -7,21 +7,20 @@ Created on Thu Jun 22 00:24:56 2017
 
 """
 break word into a valid combination of simple words
+reference: 
 """
 
 dictionary = set(["go", "dcat", "dog", "cat", "god", "goddog", "catdoggod", "goddogbat", "catcat"])
 
 def wordbreak(s):
-    return wordbreakUtil(s, len(s))
-
-def wordbreakUtil(s, n):
+    n = len(s)
     for i in xrange(1, n+1):
-        prefix = s[0:i]
+        prefix = s[:i]
         
         if prefix in dictionary:
             if i == n:
                 return 1
-            l = wordbreakUtil(s[i:n], n-i)
+            l = wordbreak(s[i:])
             if l:
                 return l + 1
     return None
@@ -30,12 +29,11 @@ if __name__ == '__main__':
     output = []
 
     for word in dictionary:
-        l = wordbreak(word)
         """
         # if l = 1; it is a simple word; 
         # if l > 1; it is a valid composite word
-        # if l is None, the input word cannot be broken into available simple words
         """
-        if l and l > 1:
+        l = wordbreak(word)
+        if l > 1:
             output.append(word)
     print (output)
