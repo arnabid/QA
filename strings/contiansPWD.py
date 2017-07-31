07c31d781d5d62fb65d9052d98b6e061ab417fcf
Arnab Saha <arnab.saha.id@gmail.com>
2017-07-31 07:53:30 -0500

    add version2

--- strings/contiansPWD.py
+++ strings/contiansPWD.py
@@ -18,14 +18,14 @@ substring method (in Java) takes constant time
 I am assuming that string slicing which is a way to do substring
 in Python takes constant time
 """
-PWD = "X"
+PWD = "xyz"
 
 def containsPWD(s):
     return PWD in s
 
-def findPassword(s):
-    # return None if the input string is empty
-    if s == "":
+def findPassword1(s):
+    # return None if the input string does not contain the password
+    if not containsPWD(s):
         return None
 
     n = len(s)
@@ -35,20 +35,40 @@ def findPassword(s):
         if containsPWD(s[:i+1]):
             pwdFound = True
             break
-    
+
     # return None if the password does not exist in string s
     if pwdFound is False:
         return None
-    
+
     # advance j from right to left starting from i till you get the actual password
     for j in xrange(i,-1,-1):
         if containsPWD(s[j:i+1]):
             return s[j:i+1]
 
+def findPassword2(s):
+    # return None if the input string does not contain the password
+    if not containsPWD(s):
+        return None
+
+    n = len(s)
+    i, j = 0, n-1
+    status = True
+    # keep 2 pointers i, j and advance them if the remaining part of string
+    # contains the password
+    while status:
+        status =  False
+        if containsPWD(s[i+1:j+1]):
+            i += 1
+            status = True
+        if containsPWD(s[i:j]):
+            j -= 1
+            status = True
+    return s[i:j+1]
+
 
 if __name__ == '__main__':
-    s = "jfhahjhdoXEH"
-    ans = findPassword(s)
+    s = ""
+    ans = findPassword1(s)
     print (ans)
     print (ans == PWD)
 
