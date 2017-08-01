bd033415a58d58e5e939abd575e46cb570b8254c
Arnab Saha <arnab.saha.id@gmail.com>
2017-07-31 21:26:25 -0500

    modify the substring search method

--- strings/basicSubstringSearch.py
+++ strings/basicSubstringSearch.py
@@ -4,7 +4,11 @@ Created on Tue Apr  4 19:52:42 2017
 
 @author: arnab
 """
-def findSubString(s1, s2):
+
+"""
+check if s2 is a substring of s1
+"""
+def findSubString2(s1, s2):
     if s1 is None or s2 is None:
         return None
     
@@ -13,20 +17,16 @@ def findSubString(s1, s2):
         return None
     
     if n2 == 0:
-        return s1
+        return s2
     
     for i in xrange(n1-n2+1):
-        if s1[i] == s2[0]:
-            j = 0
-            while j < n2:
-                if s1[i+j] != s2[j]:
-                    break
-                j += 1
-            if j == n2:
-                return s1[i:i+j]
+        # check if the substring starting at i of length n2 == s2
+        if s1[i:i+n2] == s2:
+            return s1[i:i+n2]
     return None
+
 
 if __name__ == '__main__':
     s1 = "ABCDEF"
-    s2 = "BC"
-    print (findSubString(s1, s2))
+    s2 = "ABCDEF"
+    print (findSubString2(s1, s2))
