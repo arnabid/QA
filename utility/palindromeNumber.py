15d8daec8ad1adeaf4fefc0785b6e318f62a8b48
Arnab <arnab.saha.id@gmail.com>
2017-08-04 09:31:37 -0500

    add question to check if a number is a palindrome

---
+++ utility/palindromeNumber.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 20:08:29 2017
+@author: arnab
+"""
+
+"""
+check if a number is a palindorme
+"""
+
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0 or (x != 0 and x%10 == 0):
+        return False
+    rev = 0
+    while x > rev:
+        rev = rev*10 + x%10
+        x = x/10
+    return x == rev or x == rev/10
