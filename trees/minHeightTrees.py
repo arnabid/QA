4022995afc160b119cb0d820e2eb323a62ddfe6c
Arnab Saha <arnab.saha.id@gmail.com>
2017-08-06 08:18:58 -0500

    add the question to find minimum height trees

---
+++ trees/minHeightTrees.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  6 08:11:03 2017
+
+@author: arnab
+"""
+
+"""
+Minimum height trees
+reference: https://leetcode.com/problems/minimum-height-trees/description/
+
+Notes:
+There can be only 1/2 nodes with minimum height trees.
+Lots of applications; logistics/computer/travel networks
+
+These nodes are the critical nodes in the sense that min total cost is needed to
+reach the other nodes in the network. Any sort of message propagation will be most
+efficient from these node(s).
+"""
+
+from collections import Counter
+import Queue
+
+def findMinHeightTrees(n, edges):
+    """
+    :type n: int
+    :type edges: List[List[int]]
+    :rtype: List[int]
+    """
+    if n <= 2:
+        return list(xrange(n))
+    degree = Counter()
+    graph = Counter()
+    # build the graph and the degree map
+    for u,v in edges:
+        degree[u] += 1
+        degree[v] += 1
+        graph[u] = graph.get(u,[])+[v]
+        graph[v] = graph.get(v,[])+[u]
+    
+    # put the leaves in the queue
+    q = Queue.Queue()
+    for node in graph.keys():
+        if degree[node] == 1:
+            q.put(node)
+
+    while n > 2:
+        # prune the batch of leaves
+        for i in xrange(q.qsize()):
+            v = q.get()
+            n -= 1
+            for w in graph.get(v, []):
+                degree[w] -= 1
+                if degree[w] == 1:
+                    q.put(w)
+    res = []
+    while not q.empty():
+        res.append(q.get())
+    return res
