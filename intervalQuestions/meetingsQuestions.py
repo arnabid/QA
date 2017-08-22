fd3f93ffaa715ea709292abc462dbdd222372f75
Arnab Saha <arnab.saha.id@gmail.com>
2017-08-21 22:51:12 -0500

    add notes for the interval scheduling question

--- intervalQuestions/meetingsQuestions.py
+++ intervalQuestions/meetingsQuestions.py
@@ -14,13 +14,46 @@ attended/scheduled without overlap
 scenario: maximize the number of talks that you can attend in a seminar
 you earn points for each talk attended. Once you attend a talk,
 you cannot leave until the talk is over.
+
+example:
+|---|
+      |-----|
+            |
+
+In the case of edge-cases, where the start time = end time of a meeting,
+sorting by end times and then by start times will include all 3 meetings (correct answer)
+but not sorting by start times might incude only 2 meetings.
+
+  1
+|----|       2
+          |-----|
+            3
+       |--------|
+
+sorting by end time and then by start time also has the nice
+side-effect that it minimizes the total idle time of the resource.
+
+meeting 3 is selected instead of meeting 2 in the above example.
+
+*** also calculates the total idle time and total busy time
+       
 """
 def maxNumberMeetings(meetings):
     meetings.sort(key = lambda x: (x[1], x[0]))
+    idleTime = 0
     final_meetings = [meetings[0]]
     for meeting in meetings[1:]:
         if meeting[0] >= final_meetings[-1][1]:
+            idleTime += meeting[0] - final_meetings[-1][1]
             final_meetings.append(meeting)
+    
+    """
+    *** extra credit ***
+    totalTime = final_meetings[-1][1] - final_meetings[0][0]
+    busyTime = totalTime - idleTime
+    print "busy time = ", busyTime
+    print "idle time = ", idleTime
+    """
     return len(final_meetings)
 
 
@@ -103,6 +136,7 @@ def minMeetingRooms2(meetings):
 
 if __name__ == '__main__':
     meetings = []
+    #meetings = [[1,2], [3,5], [4,7], [6,10]]
     for i in xrange(100):
         start = random.randint(1,12)
         end = random.randint(start+3,20)
