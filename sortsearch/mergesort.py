3d6df00912de125ec3e5785c426c6a4bd74e11a7
Arnab Saha <arnab.saha.id@gmail.com>
2017-10-06 07:55:51 -0500

    Revise and add the logic to count the inversions in array

--- sortsearch/mergesort.py
+++ sortsearch/mergesort.py
@@ -7,47 +7,54 @@ Created on Sat Oct  8 10:38:47 2016
 
 """
 Merge sort: http://quiz.geeksforgeeks.org/merge-sort/
-stable sorting algorithm
+stable sorting algorithm; nlogn time, n space
+counts the number of inversions; i < j and arr[i] > arr[j]
 """
 
 import random
 
-def merge(arr, low, mid, high):
-    n1 = mid - low + 1
-    n2 = high - mid
-    
-    L = arr[low:mid+1]
-    R = arr[mid+1:high+1]
-    
-    i,j,k = 0,0,low
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
+inversions = [0]
+
+def merge(arr, temp, low, mid, high):    
+    i,j,k = low,mid+1,low
+    while i <= mid and j <= high:
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
             i += 1
         else:
-            arr[k] = R[j]
+            temp[k] = arr[j]
+            inversions[0] += mid-i+1
             j += 1
         k += 1
     
-    while i < n1:
-        arr[k] = L[i]
+    while i <= mid:
+        temp[k] = arr[i]
         i += 1
         k += 1
     
-    while j < n2:
-        arr[k] = R[j]
+    while j <= high:
+        temp[k] = arr[j]
         j += 1
         k += 1
     
+    for i in xrange(low, high+1):
+        arr[i] = temp[i]
 
-def mergesort(arr, low, high):
+def mergesort(arr, temp, low, high):
     if low < high:
-        mid = low + (high-low)/2
+        mid = low + (high-low)//2
         
-        mergesort(arr, low, mid)
-        mergesort(arr, mid+1, high)
+        mergesort(arr, temp, low, mid)
+        mergesort(arr, temp, mid+1, high)
         
-        merge(arr, low, mid, high)
+        merge(arr, temp, low, mid, high)
+
+
+def _mergesort(arr, low, high):
+    temp = [0] * (high-low+1)
+    mergesort(arr, temp, low, high)
+
+
 
 if __name__ == '__main__':
     
@@ -56,14 +63,11 @@ if __name__ == '__main__':
     
     # generate test array
     arr = [random.randint(1,20) for i in xrange(10)]
-    
-    # create copy of test array
-    res = list(arr)
-    
-    # sort test array and print result
-    mergesort(arr, 0, len(arr)-1)
     print (arr)
     
-    # sort copy and print result
-    res.sort()
-    print (res)
+    # sort test array and print result
+    _mergesort(arr, 0, len(arr)-1)
+    print (arr)
+    
+    # print the number of inversions in the array
+    print ("The number of inversions = {} ".format(inversions[0]))
