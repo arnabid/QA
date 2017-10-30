855a9aedf1039af49b4695d2ace8f9aca4bda9f8
Arnab Saha <arnab.saha@halliburton.com>
2017-10-30 10:06:31 -0500

    revise the minimum height trees question; change the method signature to make it more generic

--- trees/minHeightTrees.py
+++ trees/minHeightTrees.py
@@ -12,40 +12,27 @@ reference: https://leetcode.com/problems/minimum-height-trees/description/
 Notes:
 There can be only 1 or 2 nodes with minimum height trees.
 
-These nodes are the critical nodes in the sense that maximum cost needed to
-reach the farthest node in the network will be minimum. Any sort of message propagation
+These nodes are the critical nodes in the sense that maximum distance and total distance to
+reach the a node(or all nodes) in the network will be minimum. Any sort of message propagation
 will be most efficient from these node(s).
 """
 
 from collections import Counter
 import Queue
 
-def findMinHeightTrees(n, edges):
-    """
-    :type n: int
-    :type edges: List[List[int]]
-    :rtype: List[int]
-    """
-    if n <= 2:
-        return list(xrange(n))
+def findMinHeightTrees(graph):
+	n = len(graph)
     degree = Counter()
-    graph = Counter()
-    # build the graph and the degree map
-    for u,v in edges:
-        degree[u] += 1
-        degree[v] += 1
-        graph[u] = graph.get(u,[])+[v]
-        graph[v] = graph.get(v,[])+[u]
-    
-    # put the leaves in the queue
     q = Queue.Queue()
-    for node in graph.keys():
+    # build the degree map and put the leaves in the queue
+    for node in graph:
+        degree[node] = len(graph(node))
         if degree[node] == 1:
-            q.put(node)
+        	q.put(node)
 
     while n > 2:
-        # prune the batch of leaves
-        for i in xrange(q.qsize()):
+        # prune the batch of leaves present in the queue
+        for i in range(q.qsize()):
             v = q.get()
             n -= 1
             for w in graph.get(v, []):
