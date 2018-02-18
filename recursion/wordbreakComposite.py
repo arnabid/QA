f03bb67dbde0a3db36cc26e0eef3c3ed52f079c2
Arnab Saha <arnab.saha.id@gmail.com>
2018-02-17 20:55:50 -0600

    make the solution a particular case of wbc.py

--- recursion/wordbreakComposite.py
+++ recursion/wordbreakComposite.py
@@ -8,91 +8,33 @@ Created on Thu Jun 22 00:24:56 2017
 """
 break word into a valid combination of simple words
 reference: https://leetcode.com/problems/concatenated-words/#/description
-
-
-a good test case where the memoization is useful:
-word = aaaab
-dic = set(["a", "aa"])
-work out the code and see where the savings are achieved
 """
 
-dictionary = set(["apple", "pear", "pie", "cream", "applepie", "pearpiecake"])
+dictionary = set(["apple", "pear", "pie", "cream", "applepie"])
 
-# set of strings which cannot be broken down into 2 or more words
-# used for memoization while backtracking
-notValid = []
+def dfs(s, index, n, stack, dictionary, nvalid):
+    if index == n:
+        return True
+    if index in nvalid:
+        stack.pop()
+        return False
+    for i in range(index+1, n+1):
+        t = s[index:i]
+        if t in dictionary:
+            stack.append(i)
+            if dfs(s, i, n, stack, dictionary, nvalid):
+                return True
+    nvalid.add(index)
+    stack.pop()
+    return False
 
-def wordbreakVER(s):
-    n = len(s)
-    for i in range(1, n+1):
-        prefix = s[:i]
-        
-        if prefix in dictionary:
-            if i == n:
-                return 1
-            l = wordbreak(s[i:])
-            if l:
-                return l + 1
-    return 0
-
-
-def wordbreak(s):
-    n = len(s)
-    for i in range(1, n+1):
-        prefix = s[:i]
-        
-        if prefix in dictionary:
-            if i == n:
-                return 1
-            if s[i:] not in notValid:
-                l = wordbreak(s[i:])
-                if l:
-                    return l + 1
-                else:
-                    notValid.append(s[i:])
-            else:
-                print ("used notValid")
-    return 0
-
-
-if __name__ == '__main__':
-    output = []
-        """
-        # if l = 1; it is a simple word; 
-        # if l > 1; it is a valid composite word
-        """
-        l = wordbreak(word)
-        if l > 1:
-            print (l)
-            output.append(word)
-    print (output)
-    print (nvalid)
-
-"""
-dictionary = set(["apple", "pear", "pie", "pearcream", "cream", "applepeaf"])
-
-
-def wb(s):
-    if s == "":
-        return 0
-
-    n, i= len(s), 1
-    count = 0
-    while i < n+1:
-        if s[:i] in dictionary:
-            if i == n:
-                count = 1
-            else:
-                count = 1 + wb(s[i:])
-            break
-        i += 1
-    return count
 
 if __name__ == '__main__':
     output = []
     for word in dictionary:
-        nwords = wb(word)
-        if nwords > 1:
+        n = len(word)
+        nvalid = set()
+        stack = [0]
+        if dfs(word, 0, n, stack, dictionary, nvalid) and len(stack) > 2:
             output.append(word)
     print (output)
-"""
