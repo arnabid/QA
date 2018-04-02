c207622cc47ff70eb2d1dd403bc16aba20b0e006
Arnab Saha <arnab.saha.id@gmail.com>
2018-04-02 08:25:13 -0500

    revise question

--- recursion/subsetsSumK.py
+++ recursion/subsetsSumK.py
@@ -1,15 +1,22 @@
 """
-find the number of subsets that sum to K
+find the number of subsets of a set of numbers that sum to K
 """
 
+# returns the number of subsets of arr[i:] that sum to total
 def countsubsetsK(arr, total, i, mem):
 	key = (total, i)
+
+	#use memoization
 	if key in mem:
 		return mem[key]
+
 	if total == 0:
 		return 1
+
+	# no more elements to choose
 	if i == len(arr):
 		return 0
+
 	if arr[i] > total:
 		res = countsubsetsK(arr, total, i+1, mem)
 	else:
