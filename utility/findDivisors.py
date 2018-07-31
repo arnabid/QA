da60b8f27284ef5b8e2da1fa681903e2d3f4facc
Arnab Saha <arnab.saha.id@gmail.com>
2018-07-31 08:34:02 -0500

    Revise find divisors

--- utility/findDivisors.py
+++ utility/findDivisors.py
@@ -1,20 +1,16 @@
-
 # returns the list of divisors of n in sorted order
+
 def findDivisors(n):
-	i = 1
-	l1, l2 = [], []
+	i, l1, l2 = 1, [], []
 	while i * i <= n:
 		if n % i == 0:
-			if i == n//i:
-				l1.append(i)
-			else:
-				l1.append(i)
+			l1.append(i)
+			if i != n//i:
 				l2.append(n//i)
 		i += 1
 	l1 += l2[::-1]
 	print (l1)
 
 if __name__ == '__main__':
-	n = 31
+	n = 0
 	findDivisors(n)
-
