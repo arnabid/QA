abe8851543dad6f1f4896a16f6e2820e996d6411
Arnab Saha <arnab.saha.id@gmail.com>
2018-09-23 11:36:21 -0500

    Add interview questions

---
+++ sortsearch/interviewPrep.py
@@ -0,0 +1,21 @@
+"""
+Binary search questions
+"""
+
+"""
+koko eating bananas
+reference: https://leetcode.com/problems/koko-eating-bananas/description/
+"""
+
+def findMinK(piles, H):
+	def isPossible(k):
+		return sum(math.ceil(p/k) for p in piles) <= H
+
+	l, h = 1, max(piles)
+	while l <= h:
+		k = l + (h-l)//2
+		if isPossible(k):
+			h = k - 1
+		else:
+			l = k + 1
+	return l
