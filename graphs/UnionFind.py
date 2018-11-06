6c250795a51077db125ffec1d3ebaabed1570a49
Arnab Saha <arnab.saha.id@gmail.com>
2018-11-06 13:10:14 -0600

    revise union find

--- graphs/UnionFind.py
+++ graphs/UnionFind.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Dynamic connectivity problem: a data structure that efficiently supports
 commands of the following form on N objects:
@@ -10,7 +10,8 @@ Union-Find class; union based on rank
 and find() also does path compression.
 The path compression step reduces the height of a tree by 1/2
 TODO: Finish union by height
-'''
+"""
+
 
 class UF(object):
     def __init__(self, sites):
@@ -22,32 +23,35 @@ class UF(object):
         #initialize the id and sz arrays
         for i in sites:
             self.id[i] = i
-            self.sz[i] = 1
             self.max[i] = i
+            self.sz[i] = 1
 
+
+    # returns the list of connected components
     def get_components(self):
-        # returns the components
         components = {}
-        for node in self.id.keys():
+        for node in self.id:
             r = self.find(node) # finds the root of this node
-            if r not in components: # O(1) operation
+            if r not in components:
                 components[r] = [node]
             else:
                 components[r].append(node)
 
         return list(components.values())
-            
 
+
+    # returns the number of connected components
     def count(self):
-        # returns the number of components
         return self.count
 
+
+    # returns a boolean to indicate if nodes p and q are connected
     def connected(self, p, q):
-        # returns a boolean to indicate if nodes p and q are connected
         return self.find(p) == self.find(q)
 
+
+    # returns the root of the component in which node p lies
     def find(self, p):
-        # returns the root of the component in which node p lies
         if not p in self.id:
             raise Exception("Node %s not present" % str(p))
         while p != self.id[p]:
@@ -55,16 +59,18 @@ class UF(object):
             p = self.id[p]
         return p
 
+
+    # returns the maximum element in the component which contains p
     def findmax(self, p):
-        # returns the maximum element in the component which contains p
         return self.max[self.find(p)]
 
+
+    # merges the 2 components in which nodes p and q lie
     def union(self, p, q):
-        # joins the 2 components in which nodes p and q lie
         proot = self.find(p)
         qroot = self.find(q)
 
-        if proot == qroot: # do nothing
+        if proot == qroot:
             return
         # do union by rank and also maintain the max element in the
         # resultant component
@@ -78,7 +84,7 @@ class UF(object):
             self.max[proot] = max(self.max[proot], self.max[qroot])
         self.count -= 1
 
-# testing
+
 if __name__ == '__main__':
     nodes = [1,2,3,4,5,6,7,8]
     uf = UF(nodes)
