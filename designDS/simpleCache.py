8874e45054641ad926d647da6d8b084117b18708
Arnab Saha <arnab.saha.id@gmail.com>
2019-02-08 10:39:37 -0600

    Revise simple cache:

--- designDS/simpleCache.py
+++ designDS/simpleCache.py
@@ -6,29 +6,40 @@ Created on Wed Jun 14 21:05:16 2017
 """
 
 """
-Design a simple cache; eviction policy is to remove a random element
+Design a simple cache; eviction policy is to remove a randomly picked item
 """
 import random
 
-class SimpleCache():
-    def __init__(self):
-        self.maxsize = 16
+class Item(object):
+    def __init__(self, key, val = None):
+        assert key is not None
+        self.key = key
+        self.val = val
+
+class SimpleCache(object):
+    def __init__(self, N):
+        self.maxsize = N
         self.cache = {}
+        self.size = 0
     
-    # gets the value associated with key in cache; else returns None
+    # gets the item associated with key in cache; else returns None
     def get(self, key):
         return self.cache.get(key, None)
 
-    # adds entry (key, value) in cache
-    def add(self, key, value):
-        # if cache is full, delete random entry in cache
-        if len(self.cache) == self.maxsize:
-            rkey = random.choice(list(self.cache.keys()))
-            del self.cache[rkey]
+    # adds new item in cache
+    def put(self, item):
+        # if cache is full, delete random item in cache
+        if self.size == self.maxsize:
+            self.evict()
 
         # insert new element in cache
-        self.cache[key] = value
+        self.cache[item.key] = item
+        self.size += 1
     
-    # returns the size of cache
-    def size(self):
-        return len(self.cache)
+    # evicts a randomly chosen item from the cache 
+    def evict(self):
+        if self.size > 0:
+            rkey = random.choice(list(self.cache.keys()))
+            del self.cache[rkey]
+            self.size -= 1
+
