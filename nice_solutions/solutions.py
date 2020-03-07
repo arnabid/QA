18e308c4cb88f7d6ccf600d0e2d696c6acd878a5
arnabid <arnab.saha.id@gmail.com>
2020-03-06 22:25:46 -0600

    use swap to make solution more compact

--- nice_solutions/solutions.py
+++ nice_solutions/solutions.py
@@ -13,7 +13,5 @@ def moveZeroes(nums: List[int]) -> None:
     insert_pos = 0
     for i in range(len(nums)):
         if nums[i] != 0:
-            nums[insert_pos] = nums[i]
-            if i > insert_pos:
-                nums[i] = 0
+            nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
             insert_pos += 1
